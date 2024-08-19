afc5538e910da1ba721c8cdcf392c637f424a4b2
jleoborges <leonardoborges7799@gmail.com>
2024-08-19 20:53:55 -0300

    inicio do desafio 1

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,15 @@
+import java.util. *;
+
+public class ContaTerminal {
+    int numero;
+    String agencia;
+    String nome_Cliente;
+    double saldo;
+
+    //Scanner sc = String;
+
+
+
+    system.out.println("");
+    
+}
