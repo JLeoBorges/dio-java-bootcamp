bba61d986a11ace3eea49b6d43453025bf730260
Leonardo Borges <79729169+JLeoBorges@users.noreply.github.com>
2024-11-28 00:23:05 -0300

    Update ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,15 +1,26 @@
-import java.util. *;
+import java.util.*;
 
 public class ContaTerminal {
-    int numero;
-    String agencia;
-    String nome_Cliente;
-    double saldo;
-
-    //Scanner sc = String;
-
-
-
-    system.out.println("");
     
+    public static void main(String[]args){
+
+    Scanner sc = new Scanner(System.in);
+    sc.useLocale(Locale.US);
+
+    System.out.print("Informe o número da conta: ");
+    int numero = sc.nextInt();
+    System.out.print("Informe a Agencia: ");
+    String agencia = sc.next();
+    sc.nextLine();
+    System.out.print("Infome o nome do proprietário: ");
+    String nomeCliente = sc.nextLine();
+    System.out.print("Saldo: ");
+    double saldo = sc.nextDouble();
+
+
+    System.out.println("Olá "+ nomeCliente + ",obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ",conta " + numero + " e seu saldo " + 
+    saldo + " já está disponível para saque");
+    
+
+}    
 }
